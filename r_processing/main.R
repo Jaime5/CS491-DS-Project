fc939e63c0dce15f38746353cab24e3198150b54
Jaime Orellana <jaime5@umbc.edu>
2018-05-16 02:11:24 -0400

    wow, strawberry fields, nothing is real..... and nothing to hung about. strawberry fields forever

--- r_processing/main.R
+++ r_processing/main.R
@@ -12,82 +12,23 @@ income_df[order(income_df$Median.Household.Income),]
 canton.crime = crime_df[crime_df$Neighborhood=="canton",]
 canton.income = income_df[income_df$Neighborhood=="canton",]
 canton.service = service_df[service_df$Neighborhood=="canton",]
+canton.ratio = nrow(canton.crime) / canton.income$Total.Population # 71 per 1000 crimes
+canton.average_wait_days = mean(canton.service$Time.Delta.in.secs) / 60 / 60 / 24
 
+canton.ratio
+
+plot(count(canton.crime, Day.of.the.Year))
 plot(count(canton.crime, Week.of.the.Year))
-# plot()
-
-# brooklyn.crime
-
-# canton.service$
-
-# neigh.high.fed_hill = crime_df[crime_df$Neighborhood=="federal hill",]
-#
-# num_of_weeks = seq(1, 53, 1)
-# #
-# # neigh.high.canton %>% group_by(num_of_weeks) %>% summarise(sum=sum(Week.of.the.Year))
-# neigh.high.canton.count = count(neigh.high.canton, Week.of.the.Year)
-# neigh.high.fed_hill.count = count(neigh.high.fed_hill, Week.of.the.Year)
-#
-# # cut(neigh.high.canton$Week.of.the.Year, breaks=53)
-# cut(neigh.high.canton$Week.of.the.Year, breaks=num_of_weeks, right=TRUE)
-#
-# x = as.data.frame(table(neigh.high.canton$Week.of.the.Year))
-# # num_of_weeks = neigh.high.canton.count$Week.of.the.Year
-# #
-#
-# x2 = as.data.frame(table(neigh.high.fed_hill$Week.of.the.Year))
-#
-# # neigh.high.canton.count
-#
-# plot_df = data.frame(num_of_weeks,
-#                      neigh.high.canton.count$n,
-#                      neigh.high.fed_hill.count$n)
-#
-# plot(neigh.high.canton.count)
-# lines(neigh.high.canton.count)
-# # lines()
-# #
-# neigh.high.canton$
-#
-# # count_x_y = count(neigh.high.canton, Week.of.the.Year)
-# # count_x_y
-# # plot(count_x_y)
-# # lines()
-# # lines(count(neigh.high.canton, Week.of.the.Year))
-# # plot(count(neigh.high.canton, Day.of.the.Year))
-# # plot(count(neigh.high.canton, Week.of.the.Year))
-# #
-# # # plot(count(neigh.high.canton, Day.of.the.Year))
-# # plot(count(neigh.high.fed_hill, Week.of.the.Year))
-#
-# # Low Income: Druid Heights, Harlem Park, Barclay
-# neigh.low.druid_heights = crime_df[crime_df$Neighborhood=="druid heights",]
-#
-# neigh.high.druid_heights.count = count(neigh.low.druid_heights, Week.of.the.Year)
-#
-# plot(neigh.high.druid_heights.count)
-# lines(neigh.high.druid_heights.count)
-# plot(neigh.high.canton.count)
-#
 
 
+druid_heights.crime = crime_df[crime_df$Neighborhood=="druid heights",]
+druid_heights.income = income_df[income_df$Neighborhood=="druid heights",]
+druid_heights.service = service_df[service_df$Neighborhood=="druid heights",]
+druid_heights.ratio = nrow(druid_heights.crime) / druid_heights.income$Total.Population
+druid_heights.average_wait_days = mean(druid_heights.service$Time.Delta.in.secs) / 60 / 60 / 24
 
-# neigh.low.harlem_park = crime_df[crime_df$Neighborhood=="harlem park",]
-#
-# neigh.low.harlem_park.count = count(neigh.low.harlem_park, Week.of.the.Year)
-#
-# plot(neigh.low.harlem_park.count)
-# lines(neigh.high.druid_heights.count)
+druid_heights.ratio
+druid_heights.average_wait_days
 
-# plot(neigh.high.druid_heights.count)
-
-
-# breaks = cut(seq(0,365, by=2), Inf)
-# neigh.high.canton.table = table(neigh.high.canton$Day.of.the.Year)
-
-# class(neigh.high.canton.table)
-# plot.table(neigh.low.canton.table)
-#
-# table.cont(neigh.high.canton$Day.of.the.Year)
-# freq(neigh.high.canton, )
-# count(neigh.high.canton, 'Day.of.the.Year')
+plot(count(druid_heights.crime, Day.of.the.Year))
+plot(count(druid_heights.crime, Week.of.the.Year))
